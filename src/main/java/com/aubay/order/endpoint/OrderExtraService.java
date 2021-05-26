d9a451ba04099faf68a57d8bf662f858d725aa85
victorinno <rotc21@gmail.com>
2021-05-26 20:35:00 +0100

    acertos

--- src/main/java/com/aubay/order/endpoint/OrderExtraService.java
+++ src/main/java/com/aubay/order/endpoint/OrderExtraService.java
@@ -8,7 +8,10 @@ import com.aubay.order.entity.Status;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import java.time.LocalDateTime;
 
